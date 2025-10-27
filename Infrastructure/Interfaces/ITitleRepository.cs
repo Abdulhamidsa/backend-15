8a7020ef93a2a5cab7f46141dd31079874064309
Abdulhamidsa <alsaati.abdulhamid@gmail.com>
2025-10-27 13:19:32 +0100

    Fix namespace in ITitleRepository and clean up TitleRepository formatting

--- Infrastructure/Interfaces/ITitleRepository.cs
+++ Infrastructure/Interfaces/ITitleRepository.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Infrastructure.Models;
 
-namespace Application.Interfaces
+namespace Infrastructure.Interfaces
 {
     public interface ITitleRepository
     {
--- Infrastructure/repositories/TitleRepository.cs
+++ Infrastructure/repositories/TitleRepository.cs
@@ -2,9 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using Infrastructure.Data;
 using Infrastructure.Models;
 using Infrastructure.Interfaces;
-{
-    
-}
+
 namespace Infrastructure.Repositories
 {
     public class TitleRepository : ITitleRepository
@@ -16,12 +14,11 @@ namespace Infrastructure.Repositories
             _context = context;
         }
 
-
-        async Task<IEnumerable<Title>> GetAllAsync()
+        public async Task<IEnumerable<Title>> GetAllAsync() 
         {
-            return await _context.Titles.FromSqlRaw("SELECT * FROM get_all_movies()").ToListAsync();
+            return await _context.Titles
+                                 .FromSqlRaw("SELECT * FROM get_all_movies()")
+                                 .ToListAsync();
         }
-
-
     }
 }
