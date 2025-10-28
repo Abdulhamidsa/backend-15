fa97c76290c3a3c76928ab24c2e8301e92eb470d
ziakhan82 <ziaullahhassan8@gmail.com>
2025-10-28 18:46:12 +0100

    adding models to the project

---
+++ Domain/Models/Name.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Name
+    {
+        public string Nconst { get; set; } = null!;
+        public string PrimaryName { get; set; } = null!;
+        public int? BirthYear { get; set; }
+        public int? DeathYear { get; set; }
+    }
+}
