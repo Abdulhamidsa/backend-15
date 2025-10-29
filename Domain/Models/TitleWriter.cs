891deb45e396be8c851e7a0e682d225475260889
amena21 <amenaakhterchy@gmail.com>
2025-10-29 15:14:52 +0100

    adding writer model

---
+++ Domain/Models/TitleWriter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class TitleWriter
+    {
+        public string Tconst { get; set; } = null!;
+        public string Nconst { get; set; } = null!;
+
+        public Title Title { get; set; } = null!;
+        public Name Writer { get; set; } = null!;
+    }
+}
