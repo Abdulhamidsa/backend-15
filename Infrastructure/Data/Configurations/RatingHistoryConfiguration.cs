6ed1a2e17993f137b6cda67f3437925e7565e07d
ziakhan82 <ziaullahhassan8@gmail.com>
2025-10-29 23:26:51 +0100

    added configuration fo rname rating and rating history

---
+++ Infrastructure/Data/Configurations/RatingHistoryConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Configurations
+{
+    public class RatingHistoryConfiguration : IEntityTypeConfiguration<RatingHistory>
+    {
+        public void Configure(EntityTypeBuilder<RatingHistory> entity)
+        {
+            entity.ToTable("rating_history");
+            entity.HasKey(e => new { e.UserId, e.Tconst });
+        }
+    }
+}
