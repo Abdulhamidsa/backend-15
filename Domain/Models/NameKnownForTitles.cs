b8c87099c0f6394de1041633bb96dc629c84847f
Abdulhamidsa <alsaati.abdulhamid@gmail.com>
2025-10-29 12:57:49 +0100

    Merge branch 'main' of https://github.com/Abdulhamidsa/backend-15

--- Domain/Models/NameKnownForTitles.cs
+++ Domain/Models/NameKnownForTitles.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 namespace Domain.Models
-
-public class NameKnownForTitles
 {
-    public required string Nconst { get; set; }      
-    public required string Tconst { get; set; }     
-    public NameBasics? Name { get; set; }
-    public TitleBasics? Title { get; set; }
+
+    public class NameKnownForTitles
+    {
+        public required string Nconst { get; set; }
+        public required string Tconst { get; set; }
+        public Name Name { get; set; }
+        public Title Title { get; set; }
+    }
 }
--- Domain/Models/TitleAkas.cs
+++ Domain/Models/TitleAkas.cs
@@ -1,18 +1,18 @@
 ﻿using System;
 namespace Domain.Models
 {
+
     public class TitleAkas
     {
-    public required string TitleId { get; set; }           // FK to TitleBasics
-    public required int Ordering { get; set; }             // Display order
-    public required string Title { get; set; }             // Alternate title
-    public string? Region { get; set; }                    // Country/region code
-    public string? Language { get; set; }                  // Language code
-    public string? Types { get; set; }                     // e.g., "alternative", "working"
-    public string? Attributes { get; set; }                // e.g., "IMAX", "DVD title"
-    public bool IsOriginalTitle { get; set; }              // True if it's the original title
+        public string TitleId { get; set; } = null!;       
+        public int Ordering { get; set; }             // Display order
+        public string Title { get; set; }             // Alternate title
+        public string? Region { get; set; }                    // Country/region code
+        public string? Language { get; set; }                  // Language code
+        public string? Types { get; set; }                     // e.g., "alternative", "working"
+        public string? Attributes { get; set; }                // e.g., "IMAX", "DVD title"
+        public bool IsOriginalTitle { get; set; }              // True if it's the original title
 
-    public TitleBasics? TitleBasics { get; set; }
+        public Title TitleRef { get; set; } = null!;
     }
 }
-
