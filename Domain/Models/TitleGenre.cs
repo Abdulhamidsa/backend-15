1e6729d544cd731738b23e2bee6eb05c348ad725
ziakhan82 <ziaullahhassan8@gmail.com>
2025-10-29 12:39:20 +0100

    refactoring the code for models

---
+++ Domain/Models/TitleGenre.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class TitleGenre
+    {
+        public string Tconst { get; set; } = null!;
+        public int GenreId { get; set; }
+
+        public Title Title { get; set; } = null!;
+        public Genre Genre { get; set; } = null!;
+    }
+}
