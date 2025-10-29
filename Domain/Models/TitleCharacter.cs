fbd2df5288c954c3690c62235f131228404ca73a
Rumana Afrin Rumi <rumana.afrin1995@gmail.com>
2025-10-29 15:38:57 +0100

    Models

---
+++ Domain/Models/TitleCharacter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+   public class TitleCharacter
+    {
+        public string Tconst { get; set; } = null!;
+        public string Nconst { get; set; } = null!;
+        public string Character { get; set; } = null!;
+
+        public Title Title { get; set; } = null!;
+        public Name Person { get; set; } = null!;
+    }
+}
---
+++ Domain/Models/TitlePrincipal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class TitlePrincipal
+    {
+        public string Tconst { get; set; } = null!;
+        public string Nconst { get; set; } = null!;
+        public int Ordering { get; set; }
+        public string? Category { get; set; }
+        public string? Job { get; set; }
+
+        public Title Title { get; set; } = null!;
+        public Name Person { get; set; } = null!;
+    }
+}
