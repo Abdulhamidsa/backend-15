64411d7021274563aea609312091558e6a4f7a55
ziakhan82 <ziaullahhassan8@gmail.com>
2025-10-29 16:01:19 +0100

    adding rating history

--- Domain/Models/RatingHistory.cs
+++ Domain/Models/RatingHistory.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace Domain.Models
 {
-    internal class RatingHistory
+    public class RatingHistory
     {
+
+        public long UserId { get; set; }
+        public string Tconst { get; set; } = null!;
+        public short? PreviousRating { get; set; }
+        public short NewRating { get; set; }
+        public DateTime ChangedAt { get; set; } = DateTime.UtcNow;
+
+        public User User { get; set; } = null!;
+        public Title Title { get; set; } = null!;
     }
 }
