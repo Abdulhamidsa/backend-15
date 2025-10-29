775e35fc22e86a107efdabe1d93a1afda2365bea
ziakhan82 <ziaullahhassan8@gmail.com>
2025-10-29 23:22:46 +0100

    added bookmark and userrating configuration

---
+++ Infrastructure/Data/Configurations/UserRatingConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Configurations
+{
+    public class UserRatingConfiguration : IEntityTypeConfiguration<UserRating>
+    {
+        public void Configure(EntityTypeBuilder<UserRating> entity)
+        {
+            entity.ToTable("user_rating");
+            entity.HasKey(e => new { e.UserId, e.Tconst });
+        }
+    }
+}
