4a87a55c38fa71d596d1810a115a5de8b671e315
ziakhan82 <ziaullahhassan8@gmail.com>
2025-10-29 23:17:30 +0100

    configuring title episode

---
+++ Infrastructure/Data/Configurations/TitleEpisodeConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Configurations
+{
+    public class TitleEpisodeConfiguration : IEntityTypeConfiguration<TitleEpisode>
+    {
+        public void Configure(EntityTypeBuilder<TitleEpisode> entity)
+        {
+            entity.ToTable("title_episode");
+            entity.HasKey(e => e.Tconst);
+        }
+    }
+}
