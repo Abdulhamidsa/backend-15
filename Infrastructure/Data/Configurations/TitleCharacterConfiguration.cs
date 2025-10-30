6b8349f1af2f6c229396a2f827bd4d768b2f8457
ziakhan82 <ziaullahhassan8@gmail.com>
2025-10-30 11:04:14 +0100

    added configuration for title character

---
+++ Infrastructure/Data/Configurations/TitleCharacterConfiguration.cs
@@ -0,0 +1,40 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Configurations
+{
+    public class TitleCharacterConfiguration : IEntityTypeConfiguration<TitleCharacter>
+    {
+        public void Configure(EntityTypeBuilder<TitleCharacter> entity)
+        {
+            entity.ToTable("title_characters");
+
+            // Composite key (tconst + nconst + character)
+            entity.HasKey(e => new { e.Tconst, e.Nconst, e.Character });
+
+            // Column mappings
+            entity.Property(e => e.Tconst).HasColumnName("tconst");
+            entity.Property(e => e.Nconst).HasColumnName("nconst");
+            entity.Property(e => e.Character).HasColumnName("character");
+
+            // Relationships
+            entity.HasOne(e => e.Title)
+                  .WithMany(t => t.Characters)
+                  .HasForeignKey(e => e.Tconst)
+                  .OnDelete(DeleteBehavior.Cascade);
+
+            entity.HasOne(e => e.Person)
+                  .WithMany(n => n.Characters)
+                  .HasForeignKey(e => e.Nconst)
+                  .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+    
+    }
+
