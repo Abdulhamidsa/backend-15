62bd7b434ac8dff3a6cef932592ad99809f17a97
ziakhan82 <ziaullahhassan8@gmail.com>
2025-10-30 15:28:57 +0100

    added model for person meta data and and people services to the project

---
+++ Application/services/PeopleService.cs
@@ -0,0 +1,39 @@
+﻿using Application.DTOs;
+using Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.services
+{
+    public class PeopleService : IPeopleService
+    {
+        private readonly IPeopleRepository _repo;
+
+        public PeopleService(IPeopleRepository repo)
+        {
+            _repo = repo;
+        }
+        public Task<IEnumerable<CelebritySummaryDto>> GetPopularAsync()
+        {
+            var rows = await _repo.GetPopularAsync();
+
+            return rows.Select(r => new CelebritySummaryDto
+            {
+                Nconst = r.Nconst,
+                Name = r.PrimaryName,
+                PhotoUrl = r.Photo_Url,
+                Age = r.Age,
+                Rating = r.Weighted_Avg
+            });
+        
+        }
+
+        public Task<CelebrityProfileDto?> GetProfileAsync(string nconst)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Domain/Models/PersonMetadata.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class PersonMetadata
+    {
+        // PK and FK to Name (name_basics)
+        public string Nconst { get; set; } = null!;
+
+        public string? PhotoUrl { get; set; }
+        public string? Bio { get; set; }
+
+        // Navigation to Name
+        public Name Person { get; set; } = null!;
+    }
+}
