d9e2aa5eb7c006a2358b9c29b1c04469122d577d
ziakhan82 <ziaullahhassan8@gmail.com>
2025-10-30 15:19:38 +0100

    added dtos for poeple repository

---
+++ Application/DTOs/CelebritySummaryDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs
+{
+    public class CelebritySummaryDto
+    {
+        public string Nconst { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public string? PhotoUrl { get; set; }
+        public int? Age { get; set; }
+        public decimal? Rating { get; set; }
+    }
+}
--- Application/Interfaces/IPeopleRepository.cs
+++ Application/Interfaces/IPeopleRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,7 @@ namespace Application.Interfaces
 {
     public interface IPeopleRepository
     {
+        Task<IEnumerable<Name>> GetPopularAsync();
+        Task<Name> GetProfileAsync(string nconst);
     }
 }
