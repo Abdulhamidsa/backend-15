a2e625ed6325378e5e4a590a4493b0e9e5a70930
Abdulhamidsa <alsaati.abdulhamid@gmail.com>
2025-10-30 10:46:53 +0100

    refactor: clean up TitlesController by removing unused usings and enhancing response formatting

--- Api/controller/TitleController.cs
+++ Api/controller/TitleController.cs
@@ -1,18 +1,13 @@
 using Microsoft.AspNetCore.Mvc;
-using Application.Services;
-using Application.Interfaces;
 using Microsoft.AspNetCore.Authorization;
-
-
-
-
-
-
-
+using Application.DTOs;
+using Application.Interfaces;
+using Application.Common;
 
 namespace Api.Controllers
 {
     [ApiController]
+    [Authorize]
     [Route("api/[controller]")]
     public class TitlesController : ControllerBase
     {
@@ -27,14 +22,19 @@ namespace Api.Controllers
         public async Task<IActionResult> GetAll()
         {
             var titles = await _service.GetAllTitlesAsync();
-            return Ok(titles);
+            return Ok(ApiResponse<IEnumerable<TitleDto>>.Ok(titles, "Titles fetched"));
         }
 
         [HttpGet("search")]
         public async Task<IActionResult> Search([FromQuery] long userId, [FromQuery] string q)
         {
+            if (string.IsNullOrWhiteSpace(q))
+            {
+                return BadRequest(ApiResponse<IEnumerable<TitleDto>>.Fail("Search query cannot be empty"));
+            }
+
             var results = await _service.SearchTitlesAsync(userId, q);
-            return Ok(results);
+            return Ok(ApiResponse<IEnumerable<TitleDto>>.Ok(results, "Search completed successfully"));
         }
     }
 }
