01de0fa06529d64a49af6d634f082e17a7d5c5ca
ziakhan82 <ziaullahhassan8@gmail.com>
2025-11-01 00:30:28 +0100

    added dto for titles

---
+++ Application/DTOs/TitleCatalogDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs
+{
+    public class TitleCatalogDto
+    {
+        public string Id { get; set; } = null!;         // maps from Tconst
+        public string? Title { get; set; }              // PrimaryTitle
+        public int? Year { get; set; }                  // StartYear
+        public string? Type { get; set; }               // TitleType (movie, tvSeries,...)
+        public string? Poster { get; set; }             // Poster URL
+        public string? Genre { get; set; }              // "Action, Sci-Fi"
+        public int? Metascore { get; set; }
+    }
+}
