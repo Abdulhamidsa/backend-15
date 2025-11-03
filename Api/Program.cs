c00dd7238832ecdd1d28b5233e9e6d24aa8c1f06
Abdulhamidsa <alsaati.abdulhamid@gmail.com>
2025-11-03 15:44:51 +0100

    Add Swagger configuration for API documentation in development environment

--- Api/Program.cs
+++ Api/Program.cs
@@ -53,10 +53,22 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 });
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
 app.UseMiddleware<Api.Middleware.ExceptionMiddleware>();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Backend API v1");
+        c.RoutePrefix = string.Empty;
+    });
+}
 
 app.UseAuthentication();
 app.UseAuthorization();
